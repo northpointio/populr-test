ca1a198ae8d0582a4c1d887931e75345b32407d6
Attila Nagy <attila@northpoint.io>
2017-06-19 13:35:57 +0200

    *

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-/************************************************************************
-// For extensive details about creating a Populr theme, please visit
-// http://developers.populr.me/theming
-*/
-
-
-// $(document).on('pop-initialized', function(){
-//
-// });
